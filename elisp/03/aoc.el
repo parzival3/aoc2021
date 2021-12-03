47ab7ca6440118e878468d5debd544354ca44145
Enrico Tolotto <etolotto@gmail.com>
2021-12-03 23:45:38 +0100

    day 3

--- elisp/03/aoc.el
+++ elisp/03/aoc.el
@@ -20,14 +20,9 @@
 ;;; Code:
 ;;;
 
-
-(defun aoc-char-to-number (char)
-  "Return the number corresponding tho the CHAR."
-  (- char 48))
-
 (defun aoc-list-of-char-to-list-of-numbers (list-of-chars)
   "Convert a LIST-OF-CHARS to a list of numbers."
-  (mapcar #'aoc-char-to-number list-of-chars))
+  (mapcar (apply-partially #'+ -48) list-of-chars))
 
 (defun aoc-day3-list-of-string-to-array-of-bytes (list-of-strings)
   "Convert a LIST-OF-STRINGS to a multidimensional array of bites."
@@ -59,24 +54,103 @@ The list must have the same size and they must contain only numbers."
   "Convert a BOOL-VALUE to a string."
   (if bool-value "1" "0"))
 
+(defun aoc-bool-to-int (bool-value)
+  "Convert a BOOL-VALUE to a int."
+  (if bool-value 1 0))
+
+(defun aoc-calculate-gamma (byte-array)
+  "Calculate the gamma value from the BYTE-ARRAY."
+  (mapcar (apply-partially 'aoc-is-less-then (length byte-array))
+          (seq-reduce 'aoc-sum-column-wise byte-array
+                      (make-list (length (car byte-array)) 0))))
+
+(defun aoc-calculate-epsilon (byte-array)
+  "Calculate the epsilon value from the BYTE-ARRAY."
+  (mapcar #'not (aoc-calculate-gamma byte-array)))
+
+(defun aoc--boolean-list-to-byte-number (boolean-list)
+  "Return the byte value from a BOOLEAN-LIST."
+  (string-to-number (mapconcat #'aoc-bool-to-string boolean-list "") 2))
+
+(defun aoc--number-list-to-byte-number (number-list)
+  "Return the byte value from a NUMBER-LIST."
+  (string-to-number (mapconcat #'number-to-string number-list "") 2))
+
+(defun aoc--boolean-list-to-number-list (boolean-list)
+  "Return the list of ints from a BOOLEAN-LIST."
+  (mapcar #'aoc-bool-to-int boolean-list))
+
+(defun aoc--pred (value pos input)
+  (eq (nth pos input) value))
+
+(defun aoc--filter-not-matching-value-pos (byte-array pos value)
+  "Filters the row of the BYTE-ARRAY that have a different VALUE in POS."
+  (seq-filter (apply-partially #'aoc--pred value pos) byte-array))
+
+(defconst aoc-oxygen (make-symbol "aoc-oxyge"))
+(defconst aoc-co2 (make-symbol "aoc-co2"))
+
+(defun aoc--get-new-array-support-rating (byte-array pos rating)
+  "BYTE-ARRAY POS RATING."
+  (let ((value-to-check (if (eq rating 'aoc-oxygen) 1 0))
+        (function-to-use (if (eq rating 'aoc-oxygen)
+                             #'aoc-calculate-gamma
+                           #'aoc-calculate-epsilon)))
+    ;; Check for the recursive step
+    (if (length= byte-array 2)
+        (car (aoc--filter-not-matching-value-pos byte-array pos value-to-check))
+      ;; Actual recursion
+      (aoc--get-new-array-support-rating
+       ;; Create the new byte array
+       (aoc--filter-not-matching-value-pos byte-array pos (nth pos (aoc--boolean-list-to-number-list (funcall function-to-use byte-array))))
+       (1+ pos) rating))))
+
 (defun aoc-day3-solution (file-name)
   "FILE-NAME."
   (let* ((string-list (aoc-read-file-to-list file-name))
          (byte-array  (aoc-day3-list-of-string-to-array-of-bytes string-list))
-         (gamma-rate  (mapcar (apply-partially 'aoc-is-less-then (length byte-array))
-                             (seq-reduce 'aoc-sum-column-wise byte-array (make-list (length (car byte-array)) 0))))
-         (epsilon-rate (mapcar #'not gamma-rate)))
-         (* (string-to-number (mapconcat #'aoc-bool-to-string gamma-rate "") 2)
-            (string-to-number (mapconcat #'aoc-bool-to-string epsilon-rate "") 2))))
+         (gamma-rate  (aoc-calculate-gamma byte-array))
+         (epsilon-rate (aoc-calculate-epsilon byte-array)))
+    (* (aoc--boolean-list-to-byte-number gamma-rate)
+       (aoc--boolean-list-to-byte-number epsilon-rate))))
 
+(defun aoc-day3-solution-2 (file-name)
+  "FILE-NAME."
+  (let* ((string-list (aoc-read-file-to-list file-name))
+         (byte-array  (aoc-day3-list-of-string-to-array-of-bytes string-list))
+         (oxygen-rating  (aoc--get-new-array-support-rating byte-array 0 'aoc-oxygen))
+         (scrubber-rating (aoc--get-new-array-support-rating byte-array 0 'aoc-co2)))
+    (* (aoc--number-list-to-byte-number oxygen-rating)
+       (aoc--number-list-to-byte-number scrubber-rating))))
 
+(defun aoc-day3-solution-2-debug (file-name)
+  "FILE-NAME."
+  (let* ((string-list (aoc-read-file-to-list file-name))
+         (byte-array  (aoc-day3-list-of-string-to-array-of-bytes string-list))
+         (oxygen-rating  (aoc--get-new-array-support-rating byte-array 0 'aoc-oxygen))
+         (scrubber-rating (aoc--get-new-array-support-rating byte-array 0 'aoc-co2)))
+    (cons oxygen-rating
+        scrubber-rating)))
+
+;;; Variable for testing
 (defvar aoc-day3-simple-input
   (aoc-read-file-to-list "simple_input.txt"))
 
+(defvar aoc-day3-input
+  (aoc-read-file-to-list "input.txt"))
+
+(defvar aoc-byte-array
+  (aoc-day3-list-of-string-to-array-of-bytes aoc-day3-simple-input))
+
+(defvar aoc-byte-array-full
+  (aoc-day3-list-of-string-to-array-of-bytes aoc-day3-input))
 
 (aoc-read-file-to-list "simple_input.txt")
 (aoc-day3-solution "simple_input.txt")
 (aoc-day3-solution "input.txt")
+(aoc-day3-solution-2 "input.txt")
+(aoc-day3-solution-2-debug "input.txt")
+(aoc-day3-solution-2 "simple_input.txt")
 
 (provide 'aoc)
 ;;; aoc.el ends here
